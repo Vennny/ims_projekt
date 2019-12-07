b3f546d15bc8a8e7ca6370a74f2ac2450700a1c3
Václav Trampeška <33024931+Vennny@users.noreply.github.com>
2019-12-07 17:16:48 +0100

    simbil nastaven

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ main.cpp 
+	g++ -lsimlib -lm main.cpp 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <string>	
-
+#include "simlib.h"
 using namespace std;
+
+
+Facility  Box("Box");
+Histogram Table("Table",0,25,20);
+
+class Customer : public Process { 
+  double Prichod;                 // atribute of each customer
+  void Behavior() {               // --- behavoir specification ---
+    Prichod = Time;               // incoming time
+    Seize(Box);                   // start of service
+    Wait(10);                     // time of service
+    Release(Box);                 // end of service
+    Table(Time-Prichod);          // waiting and service time
+  }
+};
+
+class Generator : public Event {  // model of system's input
+  void Behavior() {               // --- behavior specification ---
+    (new Customer)->Activate();   // new customer
+    Activate(Time+Exponential(1e3/150));  //
+  }
+};
+
 
 int main (int argc, char *argv[])
 {
-
+	// default values
 	float members = 2.6;
 	float duration = 1.0;
 	float plastic = 20.3;
@@ -268,5 +291,14 @@ int main (int argc, char *argv[])
   	cout << "biological waste: " << bio << " %" << endl;
   	cout << "burnable waste: " << burnable << " %" << endl;
 
-	return 0;
+	//return 0;
+
+	  Print(" model2 - SIMLIB/C++ example\n");
+  SetOutput("model2.out");
+  Init(0,1000);              // experiment initialization for time 0..1000
+  (new Generator)->Activate(); // customer generator
+  Run();                     // simulation
+  Box.Output();              // print of results
+  Table.Output();
+  return 0;
 }
